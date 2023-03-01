c44f8bb6955179119c918ce3a9a7a82d1b53e301
Kevin Deakers <kmdeakers@mac.com>
2023-03-01 12:30:08 -0800

    got populate shows to display correct show info.

--- tvmaze.js
+++ tvmaze.js
@@ -16,27 +16,29 @@ const $searchForm = $("#searchForm");
 async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
   const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${term}`);
-
+  console.log('response.data: ', response.data)
   return response.data;
+  
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+async function populateShows(shows) {
   $showsList.empty();
-
-  for (let show of shows) {
+  let awaitShows = await shows;
+  console.log('awaitShows', awaitShows)
+  for (let show of awaitShows) {
     const $show = $(
-      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
               src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
+             <h5 class="text-primary">${show.show.name}</h5>
+             <div><small>${show.show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
